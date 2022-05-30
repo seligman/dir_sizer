8604347b403ce6370e8c1ec02d25e2d609a618ee
Scott Seligman <scott.seligman@gmail.com>
2022-05-30 12:05:13 -0700

    Clean up comment

--- aws_pager.py
+++ aws_pager.py
@@ -14,7 +14,7 @@
 # Which is equivalent to the following pattern:
 #
 # paginator = s3.get_paginator('list_objects_v2')
-# for page in paginator.paginate(Bucket='scotts-mess'):
+# for page in paginator.paginate(Bucket='example-bucket'):
 #     for cur in page.get('Contents', []):
 #         print(cur['Key'])
 
